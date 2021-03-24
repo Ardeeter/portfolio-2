eb1224f256a61177ba5acf9643f6a69f8bbe81dc
Ally Deeter <allydeeter6639@gmail.com>
2021-03-23 17:13:44 -0700

    about page with bio and skills

--- miro/src/scenes/Home/About.js
+++ miro/src/scenes/Home/About.js
@@ -10,7 +10,7 @@ function About() {
                         <Hidden xs sm md>
                             <Col lg={5}>
                                 <div className="about-image">
-                                    <div className="image overlay-image" style={{ backgroundImage: `url(/assets/images/about/me.jpg)` }}></div>
+                                    <div className="image overlay-image border border-dark" style={{ backgroundImage: `url(/assets/images/about/me.jpg)` }}></div>
                                 </div>
                             </Col>
                         </Hidden>
@@ -27,70 +27,109 @@ function About() {
                                     <p>I am recent graduate of a 16 week immmersive full-stack software engineering bootcamp at DigitalCrafts. I decided to switch careers after one summer Python course I took at my local community college. As soon as I felt the rush and satisfaction of solving my first algorithm, I knew I was hooked. I thrive on critical thinking and problem solving. I look forward to pursuing my career and using my professional skill set to show how I can be an asset to your business.</p>
                                     <p>In my free time, I don't go anywhere without my husky pit named Buddha. We enjoy adventuring in the wilderness and swimming at the beach. I also enjoy surfing, rock climbing, and kayaking.</p>
                                 </header>
-                                <Row className="pt-30">
-                                    <Col xs={12} sm={12} md={6}>
+                                <Row className="pt-30 justify-content-sm-center">
+                                    <Col xs={2} sm={1} md={1} className="m-2">
                                         <div className="el-card-icon">
                                             <div className="el-icon">
-                                                <span className="el-icon-icon ti-ruler-pencil"></span>
-                                            </div>
-                                            <div className="content">
-                                                <h5>Web design</h5>
-                                                <p>Lorem ipsum dolor sit amet Consectetur adipisicing elit.</p>
+                                                <span className="el-icon-icon"><i class="devicon-javascript-plain"></i></span>
                                             </div>
                                         </div>
                                     </Col>
-                                    <Col xs={12} sm={12} md={6}>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
                                         <div className="el-card-icon">
                                             <div className="el-icon">
-                                                <span className="el-icon-icon ti-image"></span>
-                                            </div>
-                                            <div className="content">
-                                                <h5>Graphic Design</h5>
-                                                <p>Lorem ipsum dolor sit amet Consectetur adipisicing elit.</p>
+                                                <span className="el-icon-icon"><i class="devicon-python-plain"></i></span>
                                             </div>
                                         </div>
                                     </Col>
-                                    <Col xs={12} sm={12} md={6}>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
                                         <div className="el-card-icon">
                                             <div className="el-icon">
-                                                <span className="el-icon-icon ti-brush-alt"></span>
-                                            </div>
-                                            <div className="content">
-                                                <h5>Development</h5>
-                                                <p>Lorem ipsum dolor sit amet Consectetur adipisicing elit.</p>
+                                                <span className="el-icon-icon"><i class="devicon-java-plain"></i></span>
                                             </div>
                                         </div>
                                     </Col>
-                                    <Col xs={12} sm={12} md={6}>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
                                         <div className="el-card-icon">
                                             <div className="el-icon">
-                                                <span className="el-icon-icon ti-ruler-pencil"></span>
-                                            </div>
-                                            <div className="content">
-                                                <h5>Mobile Apps</h5>
-                                                <p>Lorem ipsum dolor sit amet Consectetur adipisicing elit.</p>
+                                                <span className="el-icon-icon"><i class="devicon-nodejs-plain"></i></span>
                                             </div>
                                         </div>
                                     </Col>
-                                    <Col xs={12} sm={12} md={6}>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
                                         <div className="el-card-icon">
                                             <div className="el-icon">
-                                                <span className="el-icon-icon ti-ruler-pencil"></span>
-                                            </div>
-                                            <div className="content">
-                                                <h5>UI/UX Designer</h5>
-                                                <p>Lorem ipsum dolor sit amet Consectetur adipisicing elit.</p>
+                                                <span className="el-icon-icon"><i class="devicon-express-original"></i></span>
                                             </div>
                                         </div>
                                     </Col>
-                                    <Col xs={12} sm={12} md={6}>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
                                         <div className="el-card-icon">
                                             <div className="el-icon">
-                                                <span className="el-icon-icon ti-mobile"></span>
+                                                <span className="el-icon-icon"><i class="devicon-react-original"></i></span>
                                             </div>
-                                            <div className="content">
-                                                <h5>Marketing Online</h5>
-                                                <p>Lorem ipsum dolor sit amet Consectetur adipisicing elit.</p>
+                                        </div>
+                                    </Col>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
+                                        <div className="el-card-icon">
+                                            <div className="el-icon">
+                                                <span className="el-icon-icon"><i class="devicon-redux-original"></i></span>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
+                                        <div className="el-card-icon">
+                                            <div className="el-icon">
+                                                <span className="el-icon-icon"><i class="devicon-postgresql-plain"></i></span>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
+                                        <div className="el-card-icon">
+                                            <div className="el-icon">
+                                                <span className="el-icon-icon"><i class="devicon-sequelize-plain"></i></span>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
+                                        <div className="el-card-icon">
+                                            <div className="el-icon">
+                                                <span className="el-icon-icon"><i class="devicon-git-plain"></i></span>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
+                                        <div className="el-card-icon">
+                                            <div className="el-icon">
+                                                <span className="el-icon-icon"><i class="devicon-npm-original-wordmark"></i></span>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
+                                        <div className="el-card-icon">
+                                            <div className="el-icon">
+                                                <span className="el-icon-icon"><i class="devicon-amazonwebservices-original"></i></span>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
+                                        <div className="el-card-icon">
+                                            <div className="el-icon">
+                                                <span className="el-icon-icon"><i class="devicon-html5-plain"></i></span>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
+                                        <div className="el-card-icon">
+                                            <div className="el-icon">
+                                                <span className="el-icon-icon"><i class="devicon-css3-plain"></i></span>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                    <Col xs={2} sm={1} md={1} className="m-2">
+                                        <div className="el-card-icon">
+                                            <div className="el-icon">
+                                                <span className="el-icon-icon"><i class="devicon-bootstrap-plain"></i></span>
                                             </div>
                                         </div>
                                     </Col>
